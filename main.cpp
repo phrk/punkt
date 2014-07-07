aac55d8f8f6dcc28d093111c402b6a546978c81e
Artur Gilmutdinov <arturg000@gmail.com>
2014-07-07 17:44:11 +0400

    Added pid saving

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "hiconfig.h"
 #include "punktd.h"
 
+#include <fstream>
+#include <iostream>
+
 int main (int argc, char** argv) {
 	
 	Punktd *pd;
 	
 	std::string conf_name;
 	//std::cout << "nargs: " << argc << " confif: " << argv[1] << std::endl <<  std::endl;
+	
+	pid_t pid = getpid();
+	
+	std::ofstream pidfile("punkt.pid");
+	pidfile << pid << std::endl;
+	pidfile.close();
 	
 	if (argc <= 1)
 		conf_name = "punkt.conf";
@@ -17,6 +26,12 @@ int main (int argc, char** argv) {
 		pd = new Punktd(conf_name);
 	} catch (const char *s) {
 		std::cout << "::main exception: " << s << std::endl;
+		exit(0);
+	} catch (std::string &_s) {
+		std::cout << "::main exception: " << _s << std::endl;
+		exit(0);
+	} catch (std::string *_s) {
+		std::cout << "::main exception: " << *_s << std::endl;
 		exit(0);
 	}
 	pd->join();
