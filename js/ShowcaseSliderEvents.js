5ae5b717c0913a6385270370ab4e1c49ed0fb5a3
Artur Gilmutdinov <arturg000@gmail.com>
2014-07-29 20:53:38 +0400

    Disabled sending item disp events

--- js/ShowcaseSliderEvents.js
+++ js/ShowcaseSliderEvents.js
@@ -16,7 +16,7 @@ function evShowcaseSliderDisp(punkturl, pid, adid) {
 
 function evShowcaseSliderItemDisp(punkturl, pid, adid, itemid) {
 	
-	xmlHttp = new XMLHttpRequest();
+/*	xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "GET", punkturl + "?evtype=fev&fid=1&ev=itemdisp&pid=" + pid + "&adid=" + adid + "&item=" + itemid, true);
 	
 	xmlHttp.onload = function (e) {
@@ -26,5 +26,5 @@ function evShowcaseSliderItemDisp(punkturl, pid, adid, itemid) {
 			}
 		}
 	};
-	xmlHttp.send( null );
+	xmlHttp.send( null );*/
 }
